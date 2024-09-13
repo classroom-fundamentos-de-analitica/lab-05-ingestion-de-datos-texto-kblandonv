4d30bd6db1850bed52332b17f6636dde33ede899
kblandonv <kblandonv@unal.edu.co>
2024-09-13 13:36:46 -0500

    Fix dataset file references and update autograding workflow

--- test.py
+++ test.py
@@ -7,25 +7,25 @@ import pandas as pd
 if not os.path.exists("train_dataset.csv"):
     raise FileNotFoundError("File 'train_dataset.csv' not found")
 
-train_dataset = pd.read_csv("train_dataset.txt")
+train_dataset = pd.read_csv("train_dataset.csv")
 
 assert train_dataset.columns[0] == "phrase"
 assert train_dataset.columns[1] == "sentiment"
 
 counts = train_dataset["sentiment"].value_counts()
 
-assert counts["neural"] == 1117
+assert counts["neutral"] == 1117
 assert counts["positive"] == 458
 assert counts["negative"] == 236
 
 
-if not os.path.exists("test_dataset.txt"):
+if not os.path.exists("test_dataset.csv"):
     raise FileNotFoundError("File 'test_dataset.csv' not found")
 
 test_dataset = pd.read_csv("test_dataset.csv")
 
-counts = train_dataset["sentiment"].value_counts()
+counts = test_dataset["sentiment"].value_counts()
 
-assert counts["neural"] == 274
+assert counts["neutral"] == 274
 assert counts["positive"] == 112
 assert counts["negative"] == 67
